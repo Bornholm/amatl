277e7906bb2501513c5ae501a7c8533ea7088888
William Petit <wpetit@cadoles.com>
2024-01-31 13:12:25 +0100

    feat: use pipelines for render commands

---
+++ pkg/command/cli/render/renderer.go
@@ -0,0 +1,54 @@
+package render
+
+import (
+	"github.com/Bornholm/amatl/pkg/markdown/directive"
+	"github.com/Bornholm/amatl/pkg/markdown/directive/include"
+	"github.com/Bornholm/amatl/pkg/markdown/renderer/markdown"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/util"
+)
+
+func newMarkdownRenderer() renderer.Renderer {
+	render := markdown.NewRenderer()
+
+	render.AddOptions(
+		renderer.WithNodeRenderers(
+			util.Prioritized(
+				directive.NewRenderer(
+					directive.WithRenderer(
+						include.Type,
+						&include.NodeRenderer{
+							Cache:    cache,
+							Renderer: render,
+						},
+					),
+				), 0,
+			),
+		),
+		markdown.WithNodeRenderer(
+			directive.KindDirective,
+			directive.NewMarkdownNodeRenderer(
+				directive.WithMarkdownDirectiveRenderer(
+					include.Type,
+					&include.MarkdownRenderer{
+						Cache: cache,
+					},
+				),
+			),
+		),
+	)
+
+	return render
+}
+
+func newHTMLRenderer() renderer.Renderer {
+	markdown := goldmark.New(
+		goldmark.WithExtensions(
+			extension.GFM,
+		),
+	)
+
+	return markdown.Renderer()
+}
