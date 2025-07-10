d4e4a375c705b6ba00e29ff1a2b3720440761081
William Petit <wpetit@cadoles.com>
2025-07-10 14:38:00 +0200

    chore: add windows debugging utilities

--- misc/make/windows.mk
+++ misc/make/windows.mk
@@ -1,15 +1,24 @@
 
-run-windows:
+misc/windows/oem/ssh.pub:
+	echo "Copy your public SSH key here." > misc/windows/oem/ssh.pub
+
+run-windows: misc/windows/oem/ssh.pub
 	docker run -it --rm \
 		--name amatl-windows \
-		-p 8006:8006 --device=/dev/kvm \
+		-p 8006:8006 \
+		-p 2424:22 \
+		--device=/dev/kvm \
 		--device=/dev/net/tun \
 		--cap-add NET_ADMIN \
 		-v "$(CURDIR)/windows:/storage" \
 		-v "$(CURDIR):/data/amatl" \
+		-v "$(CURDIR)/misc/windows/oem:/oem" \
 		-e LANGUAGE=French -e REGION=fr-FR -e KEYBOARD=fr-FR \
 		--stop-timeout 120 \
 		dockurr/windows
+
+ssh-windows:
+	ssh docker@127.0.0.1 -p 2424
 
 reset-windows:
 	sudo rm -rf "$(CURDIR)/windows"
---
+++ misc/windows/oem/install.bat
@@ -0,0 +1,2 @@
+powershell.exe -ExecutionPolicy Bypass -File "C:\OEM\enable_ssh_windows.ps1" -SSHPublicKey "C:\OEM\ssh.pub"
+powershell.exe -ExecutionPolicy Bypass -File "C:\OEM\install_chrome.ps1"
