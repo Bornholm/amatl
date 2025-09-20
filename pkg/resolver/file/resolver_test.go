d91d610ce46df8d10256b677edba244ca9dcc198
William Petit <wpetit@cadoles.com>
2025-09-20 16:17:33 +0200

    chore: add file resolver test

--- pkg/resolver/file/resolver_test.go
+++ pkg/resolver/file/resolver_test.go
@@ -1,11 +1,67 @@
 package file
 
 import (
+	"context"
+	"io"
 	"net/url"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/pkg/errors"
 )
+
+func TestResolver(t *testing.T) {
+	resolver := NewResolver()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("%+v", errors.WithStack(err))
+	}
+
+	filepath.Join()
+
+	urls := []string{
+		"./testdata/test.txt",
+		"file://testdata/test.txt",
+		filepath.Join(workingDirectory, "testdata/test.txt"),
+	}
+
+	resolve := func(rawURL string) {
+		t.Run(rawURL, func(t *testing.T) {
+			u, err := url.Parse(rawURL)
+			if err != nil {
+				t.Errorf("%+v", errors.WithStack(err))
+				return
+			}
+
+			reader, err := resolver.Resolve(ctx, u)
+			if err != nil {
+				t.Errorf("%+v", errors.WithStack(err))
+				return
+			}
+
+			defer reader.Close()
+
+			data, err := io.ReadAll(reader)
+			if err != nil {
+				t.Errorf("%+v", errors.WithStack(err))
+				return
+			}
+
+			if e, g := "foo", string(data); e != g {
+				t.Errorf("data: expected '%v', got '%v'", e, g)
+			}
+		})
+	}
+
+	for _, rawURL := range urls {
+		resolve(rawURL)
+	}
+}
 
 func TestWindowsToFilePath(t *testing.T) {
 	path := `C:\Users\Docker\Desktop\examples\document\document.md`
